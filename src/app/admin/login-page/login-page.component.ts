c8bd100117f27401acc98a8540e3a403404cd1c7
Denis <oggyqwe@gmail.com>
2024-10-22 16:38:41 +0400

    add validation in form Ligin

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit {
 
+  
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
+
+  submit() {
+    console.log('submit')
+  }
+
+  public form = new FormGroup({
+    login: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
 }
