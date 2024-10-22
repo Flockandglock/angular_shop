685e5a67d4d95eede0c6fecbf7f19a1364a17d32
Denis <oggyqwe@gmail.com>
2024-10-22 15:10:07 +0400

    edit component

--- src/app/cart-page/cart-page.component.ts
+++ src/app/cart-page/cart-page.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-cart-page',
-  standalone: true,
-  imports: [],
   templateUrl: './cart-page.component.html',
   styleUrl: './cart-page.component.scss'
 })
